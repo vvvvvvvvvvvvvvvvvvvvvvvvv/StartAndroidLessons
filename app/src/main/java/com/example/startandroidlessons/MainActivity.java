7ff03de69a916173a37cc3c1d60ad2d9b749bc85
vvvvvvvvvvvvvvvvvvvvvvvvv <31472677+vvvvvvvvvvvvvvvvvvvvvvvvv@users.noreply.github.com>
2021-02-16 13:28:48 +0300

    Menu and context menu

--- app/src/main/java/com/example/startandroidlessons/MainActivity.java
+++ app/src/main/java/com/example/startandroidlessons/MainActivity.java
@@ -1,14 +1,104 @@
 package com.example.startandroidlessons;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+    final int MENU_COLOR_RED = 1;
+    final int MENU_COLOR_GREEN = 2;
+    final int MENU_COLOR_BLUE = 3;
 
+    final int MENU_SIZE_22 = 4;
+    final int MENU_SIZE_26 = 5;
+    final int MENU_SIZE_30 = 6;
+    TextView tvColor, tvSize;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvColor = (TextView) findViewById(R.id.tvColor);
+        tvSize = (TextView) findViewById(R.id.tvSize);
+
+        // context menu should be created for tvColor and tvSize
+        registerForContextMenu(tvColor);
+        registerForContextMenu(tvSize);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       /* menu.add("menu1");
+        menu.add("menu2");
+        menu.add("menu3");
+        menu.add("menu4");*/
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        switch (v.getId()) {
+            case R.id.tvColor:
+                menu.add(0, MENU_COLOR_RED, 0, "Red");
+                menu.add(0, MENU_COLOR_GREEN, 0, "Green");
+                menu.add(0, MENU_COLOR_BLUE, 0, "Blue");
+                break;
+            case R.id.tvSize:
+                menu.add(0, MENU_SIZE_22, 0, "22");
+                menu.add(0, MENU_SIZE_26, 0, "26");
+                menu.add(0, MENU_SIZE_30, 0, "30");
+                break;
+
+
+    }
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case MENU_COLOR_RED:
+                tvColor.setTextColor(Color.RED);
+                tvColor.setText("Text color = red");
+                break;
+            case MENU_COLOR_GREEN:
+                tvColor.setTextColor(Color.GREEN);
+                tvColor.setText("Text color = green");
+                break;
+            case MENU_COLOR_BLUE:
+                tvColor.setTextColor(Color.BLUE);
+                tvColor.setText("Text color = blue");
+                break;
+            // menu items for tvSize
+            case MENU_SIZE_22:
+                tvSize.setTextSize(22);
+                tvSize.setText("Text size = 22");
+                break;
+            case MENU_SIZE_26:
+                tvSize.setTextSize(26);
+                tvSize.setText("Text size = 26");
+                break;
+            case MENU_SIZE_30:
+                tvSize.setTextSize(30);
+                tvSize.setText("Text size = 30");
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
     }
 }
