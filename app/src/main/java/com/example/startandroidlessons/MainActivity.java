e1117f6c7e8fff7879e9de995e288405c600ac14
vvvvvvvvvvvvvvvvvvvvvvvvv <31472677+vvvvvvvvvvvvvvvvvvvvvvvvv@users.noreply.github.com>
2021-02-18 16:51:32 +0300

    Intent and Intent filter

--- app/src/main/java/com/example/startandroidlessons/MainActivity.java
+++ app/src/main/java/com/example/startandroidlessons/MainActivity.java
@@ -2,83 +2,42 @@ package com.example.startandroidlessons;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.app.ShareCompat;
-
-import android.animation.Animator;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.ContextMenu;
-import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.RadioGroup;
-import android.widget.SeekBar;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import javax.xml.transform.sax.TemplatesHandler;
 
 
 
-public class MainActivity extends AppCompatActivity {
 
-    final String TAG = "States";
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.button);
+        Button btnTime = (Button) findViewById(R.id.btnTime);
+        Button btnDate = (Button) findViewById(R.id.btnDate);
 
-        button.setOnClickListener((view) -> {
-            Intent intent = new Intent(this, SecondActivity.class);
-            startActivity(intent);
-        });
-        Log.d(TAG, "MainActivity: onCreate()");
-    }
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "MainActivity: onStart()");
+        btnTime.setOnClickListener(this);
+        btnDate.setOnClickListener(this);
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "MainActivity: onResume()");
-    }
+    public void onClick(View v) {
+        Intent intent;
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "MainActivity: onPause()");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "MainActivity: onStop()");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "MainActivity: onDestroy()");
-    }
-
-    @Override
-    protected void onRestart() {
-        Log.d(TAG, "MainActivity: onRestart()");
-        super.onRestart();
+        switch(v.getId()) {
+            case R.id.btnTime:
+                intent = new Intent("com.example.intent.action.showtime");
+                startActivity(intent);
+                break;
+            case R.id.btnDate:
+                intent = new Intent("com.example.intent.action.showdate");
+                startActivity(intent);
+                break;
+        }
     }
 }
