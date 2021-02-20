6ad79e72e4e175a3fb565e939a9a8395169f13cf
vvvvvvvvvvvvvvvvvvvvvvvvv <31472677+vvvvvvvvvvvvvvvvvvvvvvvvv@users.noreply.github.com>
2021-02-20 23:08:19 +0300

    Base operations with sqlite

--- app/src/main/java/com/example/startandroidlessons/MainActivity.java
+++ app/src/main/java/com/example/startandroidlessons/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.startandroidlessons;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,14 +14,18 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button btnWeb;
-    Button btnMap;
-    Button btnCall;
+    final String LOG_TAG = "myLogs";
+
+    Button btnAdd, btnRead, btnClear;
+    EditText etName, etEmail;
+
+    DBHelper dbHelper;
 
     /** Called when the activity is first created. */
     @Override
@@ -25,36 +33,64 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnWeb = (Button) findViewById(R.id.btnWeb);
-        btnMap = (Button) findViewById(R.id.btnMap);
-        btnCall = (Button) findViewById(R.id.btnCall);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(this);
 
-        btnWeb.setOnClickListener(this);
-        btnMap.setOnClickListener(this);
-        btnCall.setOnClickListener(this);
+        btnRead = (Button) findViewById(R.id.btnRead);
+        btnRead.setOnClickListener(this);
+
+        btnClear = (Button) findViewById(R.id.btnClear);
+        btnClear.setOnClickListener(this);
+
+        etName = (EditText) findViewById(R.id.etName);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+
+        dbHelper = new DBHelper(this);
     }
 
     @Override
     public void onClick(View v) {
-        Intent intent;
+        ContentValues cv = new ContentValues();
+        String name = etName.getText().toString();
+        String email = etEmail.getText().toString();
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
         switch (v.getId()) {
-            case R.id.btnWeb:
-                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/"));
-                startActivity(intent);
+            case R.id.btnAdd:
+                Log.d(LOG_TAG, "--- Insert in mytable: ---");
+
+                cv.put("name", name);
+                cv.put("email", email);
+                long rowID = db.insert("mytable", null, cv);
+                Log.d(LOG_TAG, "row inserted, ID = " + rowID);
                 break;
-            case R.id.btnMap:
-                intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("geo:55.754283,37.62002"));
-                startActivity(intent);
+            case R.id.btnRead:
+                Log.d(LOG_TAG, "--- Rows in mytable: ---");
+                Cursor c = db.query("mytable", null, null, null, null, null, null);
+
+                if (c.moveToFirst()) {
+                    int idColIndex = c.getColumnIndex("id");
+                    int nameColIndex = c.getColumnIndex("name");
+                    int emailColIndex = c.getColumnIndex("email");
+
+                    do {
+                        Log.d(LOG_TAG,
+                                "ID = " + c.getInt(idColIndex) +
+                                        ", name = " + c.getString(nameColIndex) +
+                                        ", email = " + c.getString(emailColIndex));
+                    } while (c.moveToNext());
+                } else
+                    Log.d(LOG_TAG, "0 rows");
+                c.close();
                 break;
-            case R.id.btnCall:
-                intent = new Intent(Intent.ACTION_DIAL);
-                intent.setData(Uri.parse("tel:12345"));
-                startActivity(intent);
+            case R.id.btnClear:
+                Log.d(LOG_TAG, "--- Clear mytable: ---");
+                int clearCount = db.delete("mytable", null, null);
+                Log.d(LOG_TAG, "deleted rows count = " + clearCount);
                 break;
         }
-    }
+        dbHelper.close();
 
 
+}
 }
