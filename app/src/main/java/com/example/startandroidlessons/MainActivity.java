e999528459f162ab560015deedee9a8b1936ba55
vvvvvvvvvvvvvvvvvvvvvvvvv <31472677+vvvvvvvvvvvvvvvvvvvvvvvvv@users.noreply.github.com>
2021-02-21 19:44:29 +0300

    LayoutInflater

--- app/src/main/java/com/example/startandroidlessons/MainActivity.java
+++ app/src/main/java/com/example/startandroidlessons/MainActivity.java
@@ -1,113 +1,24 @@
 package com.example.startandroidlessons;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.ContentValues;
-import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.LinearLayout;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    final String LOG_TAG = "myLogs";
+public class MainActivity extends AppCompatActivity  {
 
-    Button btnAdd, btnRead, btnClear, btnUpd, btnDel;
-    EditText etName, etEmail, etID;
-
-    DBHelper dbHelper;
-
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnAdd = (Button) findViewById(R.id.btnAdd);
-        btnAdd.setOnClickListener(this);
-
-        btnRead = (Button) findViewById(R.id.btnRead);
-        btnRead.setOnClickListener(this);
-        btnUpd = (Button) findViewById(R.id.btnUpdate);
-        btnUpd.setOnClickListener(this);
-
-        btnDel = (Button) findViewById(R.id.btnDelete);
-        btnDel.setOnClickListener(this);
-
-        btnClear = (Button) findViewById(R.id.btnClear);
-        btnClear.setOnClickListener(this);
-
-        etName = (EditText) findViewById(R.id.etName);
-        etEmail = (EditText) findViewById(R.id.etEmail);
-        etID = (EditText) findViewById(R.id.id);
-
-        dbHelper = new DBHelper(this);
+        LayoutInflater layoutInflater = getLayoutInflater();
+        LinearLayout linLayout = findViewById(R.id.linLayout);
+        View view = layoutInflater.inflate(R.layout.text, linLayout, false);
+        linLayout.addView(view);
     }
 
-    @Override
-    public void onClick(View v) {
-        ContentValues cv = new ContentValues();
-        String name = etName.getText().toString();
-        String email = etEmail.getText().toString();
-        String id = etID.getText().toString();
 
-        SQLiteDatabase db = dbHelper.getReadableDatabase();
-        switch (v.getId()) {
-            case R.id.btnAdd:
-                Log.d(LOG_TAG, "--- Insert in mytable: ---");
-
-                cv.put("name", name);
-                cv.put("email", email);
-                long rowID = db.insert("mytable", null, cv);
-                Log.d(LOG_TAG, "row inserted, ID = " + rowID);
-                break;
-            case R.id.btnRead:
-                Log.d(LOG_TAG, "--- Rows in mytable: ---");
-                Cursor c = db.query("mytable", null, null, null, null, null, null);
-
-                if (c.moveToFirst()) {
-                    int idColIndex = c.getColumnIndex("id");
-                    int nameColIndex = c.getColumnIndex("name");
-                    int emailColIndex = c.getColumnIndex("email");
-
-                    do {
-                        Log.d(LOG_TAG,
-                                "ID = " + c.getInt(idColIndex) +
-                                        ", name = " + c.getString(nameColIndex) +
-                                        ", email = " + c.getString(emailColIndex));
-                    } while (c.moveToNext());
-                } else
-                    Log.d(LOG_TAG, "0 rows");
-                c.close();
-                break;
-            case R.id.btnClear:
-                Log.d(LOG_TAG, "--- Clear mytable: ---");
-                int clearCount = db.delete("mytable", null, null);
-                Log.d(LOG_TAG, "deleted rows count = " + clearCount);
-                break;
-            case R.id.btnDelete:
-                int delete = db.delete("mytable", "id = " + id, null);
-                break;
-            case R.id.btnUpdate:
-                cv.put("name", name);
-                cv.put("email", email);
-                int update = db.update("mytable", cv,"id = ?" , new String[]{id});
-                break;
-
-
-        }
-        dbHelper.close();
-
-
-}
 }
