0a4f0d885ee8d9d1064dbc010022ab982b16d492
vvvvvvvvvvvvvvvvvvvvvvvvv <31472677+vvvvvvvvvvvvvvvvvvvvvvvvv@users.noreply.github.com>
2021-02-25 17:42:27 +0300

    GridView

--- app/src/main/java/com/example/startandroidlessons/MainActivity.java
+++ app/src/main/java/com/example/startandroidlessons/MainActivity.java
@@ -1,65 +1,33 @@
 package com.example.startandroidlessons;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cursoradapter.widget.SimpleCursorAdapter;
-import androidx.interpolator.view.animation.LinearOutSlowInInterpolator;
 
-import android.database.Cursor;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.ContextMenu;
-import android.view.LayoutInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.ProgressBar;
-import android.widget.SimpleAdapter;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import android.widget.GridView;
 
 
 public class MainActivity extends AppCompatActivity  {
-    String[] data = {"one", "two", "three", "four", "five"};
+    String[] data = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"};
 
-    /** Called when the activity is first created. */
+    GridView gvMain;
+    ArrayAdapter<String> adapter;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // адаптер
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, data);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter = new ArrayAdapter<String>(this, R.layout.item, R.id.tvText, data);
+        gvMain = (GridView) findViewById(R.id.gvMain);
+        gvMain.setAdapter(adapter);
+        adjustGridView();
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        spinner.setAdapter(adapter);
-        // заголовок
-        spinner.setPrompt("Title");
-        // выделяем элемент
-       // spinner.setSelection(2);
-        // устанавливаем обработчик нажатия
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view,
-                                       int position, long id) {
-                // показываем позиция нажатого элемента
-                Toast.makeText(getBaseContext(), "Position = " + position, Toast.LENGTH_SHORT).show();
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> arg0) {
-            }
-        });
     }
-
+    private void adjustGridView() {
+        gvMain.setNumColumns(GridView.AUTO_FIT);
+        gvMain.setColumnWidth(80);
+        gvMain.setVerticalSpacing(5);
+        gvMain.setHorizontalSpacing(5);
+    }
 
 }
