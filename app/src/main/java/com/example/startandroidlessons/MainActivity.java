e5edae505e37a64ac2473cb0b3b7a9a2eb474022
vvvvvvvvvvvvvvvvvvvvvvvvv <31472677+vvvvvvvvvvvvvvvvvvvvvvvvv@users.noreply.github.com>
2021-03-06 12:35:36 +0300

    External and Internal Storage

--- app/src/main/java/com/example/startandroidlessons/MainActivity.java
+++ app/src/main/java/com/example/startandroidlessons/MainActivity.java
@@ -1,26 +1,193 @@
 package com.example.startandroidlessons;
-import androidx.appcompat.app.AppCompatActivity;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
-import android.content.Intent;
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    FileOutputStream outputStream;
+
     final String LOG_TAG = "myLogs";
 
+    final String FILENAME = "file";
+
+    final String DIR_SD = "MyFiles";
+    final String FILENAME_SD = "fileSD";
+    private static final int REQUEST_PERMISSION_WRITE = 1001;
+    private boolean permissionGranted;
+
     /** Called when the activity is first created. */
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
     public void onclick(View v) {
-        MyObject myObj = new MyObject("text", 1);
-        Intent intent = new Intent(this, SecondActivity.class);
-        intent.putExtra(MyObject.class.getCanonicalName(), myObj);
-        Log.d(LOG_TAG, "startActivity");
-        startActivity(intent);
+        switch (v.getId()) {
+            case R.id.btnWrite:
+                writeFile();
+                break;
+            case R.id.btnRead:
+                readFile();
+                break;
+            case R.id.btnWriteSD:
+                writeFileSD();
+                break;
+            case R.id.btnReadSD:
+                readFileSD();
+                break;
+        }
+    }
+
+    void writeFile() {
+        try {
+           outputStream = openFileOutput(FILENAME, MODE_PRIVATE);
+           String text = "Abak";
+           outputStream.write(text.getBytes());
+           outputStream.close();
+            Log.d(LOG_TAG, "Файл записан");
+        } catch (FileNotFoundException e) {
+            Log.d(LOG_TAG, "Файл записан1");
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.d(LOG_TAG, "Файл записан2");
+            e.printStackTrace();
+        }
+    }
+
+    void readFile() {
+        FileInputStream fis = null;
+        try {
+            fis = openFileInput(FILENAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+            while ((text = br.readLine()) != null) {
+                sb.append(text).append("\n");
+            }
+            Log.d(LOG_TAG, sb.toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void writeFileSD() {
+        // проверяем доступность SD
+        if(!permissionGranted){
+            checkPermissions();
+            return;
+        }
+        // получаем путь к SD
+        File sdPath = Environment.getExternalStorageDirectory();
+        // добавляем свой каталог к пути
+        sdPath = new File(sdPath.getAbsolutePath() + "/" + DIR_SD);
+        // создаем каталог
+        sdPath.mkdirs();
+        // формируем объект File, который содержит путь к файлу
+        File sdFile = new File(sdPath, FILENAME_SD);
+        try {
+            // открываем поток для записи
+            BufferedWriter bw = new BufferedWriter(new FileWriter(sdFile));
+            // пишем данные
+            bw.write("Содержимое файла на SD");
+            // закрываем поток
+            bw.close();
+            Log.d(LOG_TAG, "Файл записан на SD: " + sdFile.getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void readFileSD() {
+        // проверяем доступность SD
+        if(!permissionGranted){
+            checkPermissions();
+            return;
+        }
+        // получаем путь к SD
+        File sdPath = Environment.getExternalStorageDirectory();
+        // добавляем свой каталог к пути
+        sdPath = new File(sdPath.getAbsolutePath() + "/" + DIR_SD);
+        // формируем объект File, который содержит путь к файлу
+        File sdFile = new File(sdPath, FILENAME_SD);
+        try {
+            // открываем поток для чтения
+            BufferedReader br = new BufferedReader(new FileReader(sdFile));
+            String str = "";
+            // читаем содержимое
+            while ((str = br.readLine()) != null) {
+                Log.d(LOG_TAG, str);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    // проверяем, доступно ли внешнее хранилище для чтения и записи
+    public boolean isExternalStorageWriteable(){
+        String state = Environment.getExternalStorageState();
+        return  Environment.MEDIA_MOUNTED.equals(state);
+    }
+    // проверяем, доступно ли внешнее хранилище хотя бы только для чтения
+    public boolean isExternalStorageReadable(){
+        String state = Environment.getExternalStorageState();
+        return  (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state));
+    }
+    private boolean checkPermissions(){
+        if(!isExternalStorageReadable() || !isExternalStorageWriteable()){
+            Toast.makeText(this, "Внешнее хранилище не доступно", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if(permissionCheck!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_PERMISSION_WRITE);
+            return false;
+        }
+        return true;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        switch (requestCode){
+            case REQUEST_PERMISSION_WRITE:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    permissionGranted = true;
+                    Toast.makeText(this, "Разрешения получены", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(this, "Необходимо дать разрешения", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
     }
 }
