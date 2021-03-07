e1fbf5c0d884057f7876f4bee542f88c498092e9
vvvvvvvvvvvvvvvvvvvvvvvvv <31472677+vvvvvvvvvvvvvvvvvvvvvvvvv@users.noreply.github.com>
2021-03-07 19:59:27 +0300

    Async code

--- app/src/main/java/com/example/startandroidlessons/MainActivity.java
+++ app/src/main/java/com/example/startandroidlessons/MainActivity.java
@@ -2,96 +2,54 @@ package com.example.startandroidlessons;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.util.Log;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
-import android.view.View;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends Activity {
 
-    ProgressBar pbCount;
-    TextView tvInfo;
-    CheckBox chbInfo;
-    int cnt;
-
     final String LOG_TAG = "myLogs";
-    final int max = 100;
 
-    Handler h;
+    TextView tvInfo;
 
     /** Called when the activity is first created. */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        h = new Handler();
-
-        pbCount = (ProgressBar) findViewById(R.id.pbCount);
-        pbCount.setMax(max);
-        pbCount.setProgress(0);
-
         tvInfo = (TextView) findViewById(R.id.tvInfo);
-
-        chbInfo = (CheckBox) findViewById(R.id.chbInfo);
-        chbInfo.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView,
-                                         boolean isChecked) {
-                if (isChecked) {
-
-                    tvInfo.setVisibility(View.VISIBLE);
-                    // показываем информацию
-                    h.post(showInfo);
-                } else {
-                    tvInfo.setVisibility(View.GONE);
-                    // отменяем показ информации
-                    h.removeCallbacks(showInfo);
-                }
-            }
-        });
 
         Thread t = new Thread(new Runnable() {
             public void run() {
                 try {
-                    for (cnt = 1; cnt < max; cnt++) {
-                        TimeUnit.MILLISECONDS.sleep(100);
-                        // обновляем ProgressBar
-                        h.post(updateProgress);
-                    }
+                    TimeUnit.SECONDS.sleep(2);
+                    runOnUiThread(runn1);
+                    TimeUnit.SECONDS.sleep(1);
+                    tvInfo.postDelayed(runn3, 2000);
+                    tvInfo.post(runn2);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         });
         t.start();
-
     }
 
-    // обновление ProgressBar
-    Runnable updateProgress = new Runnable() {
+    Runnable runn1 = new Runnable() {
         public void run() {
-            pbCount.setProgress(cnt);
+            tvInfo.setText("runn1");
         }
     };
 
-    // показ информации
-    Runnable showInfo = new Runnable() {
+    Runnable runn2 = new Runnable() {
         public void run() {
-            Log.d(LOG_TAG, "showInfo");
-            tvInfo.setText("Count = " + cnt);
-            // планирует сам себя через 1000 мсек
-            h.postDelayed(showInfo, 1000);
+            tvInfo.setText("runn2");
+        }
+    };
+
+    Runnable runn3 = new Runnable() {
+        public void run() {
+            tvInfo.setText("runn3");
         }
     };
 }
