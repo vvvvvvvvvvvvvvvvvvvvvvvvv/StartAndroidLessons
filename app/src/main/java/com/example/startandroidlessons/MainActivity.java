f8d36a295ab60b091da7ee23871b8f3e3af9bb9d
vvvvvvvvvvvvvvvvvvvvvvvvv <31472677+vvvvvvvvvvvvvvvvvvvvvvvvv@users.noreply.github.com>
2021-03-07 12:47:47 +0300

    Handler sendMessagePostDelay

--- app/src/main/java/com/example/startandroidlessons/MainActivity.java
+++ app/src/main/java/com/example/startandroidlessons/MainActivity.java
@@ -1,85 +1,37 @@
 package com.example.startandroidlessons;
 
-import java.util.concurrent.TimeUnit;
-
-import java.util.concurrent.TimeUnit;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.os.Message;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
     final String LOG_TAG = "myLogs";
 
-    final int STATUS_NONE = 0; // нет подключения
-    final int STATUS_CONNECTING = 1; // подключаемся
-    final int STATUS_CONNECTED = 2; // подключено
-
     Handler h;
 
-    TextView tvStatus;
-    ProgressBar pbConnect;
-    Button btnConnect;
+    Handler.Callback hc = new Handler.Callback() {
+        public boolean handleMessage(Message msg) {
+            Log.d(LOG_TAG, "what = " + msg.what);
+            return false;
+        }
+    };
 
     /** Called when the activity is first created. */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvStatus = (TextView) findViewById(R.id.tvStatus);
-        pbConnect = (ProgressBar) findViewById(R.id.pbConnect);
-        btnConnect = (Button) findViewById(R.id.btnConnect);
-
-        h = new Handler() {
-            public void handleMessage(android.os.Message msg) {
-                switch (msg.what) {
-                    case STATUS_NONE:
-                        btnConnect.setEnabled(true);
-                        tvStatus.setText("Not connected");
-                        break;
-                    case STATUS_CONNECTING:
-                        btnConnect.setEnabled(false);
-                        pbConnect.setVisibility(View.VISIBLE);
-                        tvStatus.setText("Connecting");
-                        break;
-                    case STATUS_CONNECTED:
-                        pbConnect.setVisibility(View.GONE);
-                        tvStatus.setText("Connected");
-                        break;
-                }
-            };
-        };
-        h.sendEmptyMessage(STATUS_NONE);
+        h = new Handler(hc);
+        sendMessages();
     }
 
-    public void onclick(View v) {
-        Thread t = new Thread(new Runnable() {
-            public void run() {
-                try {
-                    // устанавливаем подключение
-                    h.sendEmptyMessage(STATUS_CONNECTING);
-                    TimeUnit.SECONDS.sleep(2);
-
-                    // установлено
-                    h.sendEmptyMessage(STATUS_CONNECTED);
-
-                    // выполняется какая-то работа
-                    TimeUnit.SECONDS.sleep(3);
-
-                    // разрываем подключение
-                    h.sendEmptyMessage(STATUS_NONE);
-
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        t.start();
+    void sendMessages() {
+        Log.d(LOG_TAG, "send messages");
+        h.sendEmptyMessageDelayed(1, 1000);
+        h.sendEmptyMessageDelayed(2, 2000);
+        h.sendEmptyMessageDelayed(3, 3000);
     }
-
 }
