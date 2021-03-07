0fc6ff874a391ebfcdbb719e5c213682e75aef4f
vvvvvvvvvvvvvvvvvvvvvvvvv <31472677+vvvvvvvvvvvvvvvvvvvvvvvvv@users.noreply.github.com>
2021-03-07 12:16:14 +0300

    Handler sending message

--- app/src/main/java/com/example/startandroidlessons/MainActivity.java
+++ app/src/main/java/com/example/startandroidlessons/MainActivity.java
@@ -2,73 +2,84 @@ package com.example.startandroidlessons;
 
 import java.util.concurrent.TimeUnit;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.os.Environment;
-import android.os.Handler;
-import android.util.Log;
-import android.view.View;
+import java.util.concurrent.TimeUnit;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     final String LOG_TAG = "myLogs";
 
-    Handler h;
-    TextView tvInfo;
-    Button btnStart;
+    final int STATUS_NONE = 0; // нет подключения
+    final int STATUS_CONNECTING = 1; // подключаемся
+    final int STATUS_CONNECTED = 2; // подключено
 
+    Handler h;
+
+    TextView tvStatus;
+    ProgressBar pbConnect;
+    Button btnConnect;
+
+    /** Called when the activity is first created. */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvInfo = (TextView) findViewById(R.id.tvInfo);
-        btnStart = (Button) findViewById(R.id.btnStart);
+
+        tvStatus = (TextView) findViewById(R.id.tvStatus);
+        pbConnect = (ProgressBar) findViewById(R.id.pbConnect);
+        btnConnect = (Button) findViewById(R.id.btnConnect);
+
         h = new Handler() {
             public void handleMessage(android.os.Message msg) {
-                // обновляем TextView
-                tvInfo.setText("Закачано файлов: " + msg.what);
-                if (msg.what == 10) btnStart.setEnabled(true);
+                switch (msg.what) {
+                    case STATUS_NONE:
+                        btnConnect.setEnabled(true);
+                        tvStatus.setText("Not connected");
+                        break;
+                    case STATUS_CONNECTING:
+                        btnConnect.setEnabled(false);
+                        pbConnect.setVisibility(View.VISIBLE);
+                        tvStatus.setText("Connecting");
+                        break;
+                    case STATUS_CONNECTED:
+                        pbConnect.setVisibility(View.GONE);
+                        tvStatus.setText("Connected");
+                        break;
+                }
             };
         };
+        h.sendEmptyMessage(STATUS_NONE);
     }
 
     public void onclick(View v) {
-        switch (v.getId()) {
-            case R.id.btnStart:
-                btnStart.setEnabled(false);
-                Thread t = new Thread(new Runnable() {
-                    public void run() {
-                        for (int i = 1; i <= 10; i++) {
-                            // долгий процесс
-                            downloadFile();
-                            h.sendEmptyMessage(i);
-                            // пишем лог
-                            Log.d(LOG_TAG, "i = " + i);
-                        }
-                    }
-                });
-                t.start();
-                break;
-            case R.id.btnTest:
-                Log.d(LOG_TAG, "test");
-                break;
-            default:
-                break;
-        }
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    // устанавливаем подключение
+                    h.sendEmptyMessage(STATUS_CONNECTING);
+                    TimeUnit.SECONDS.sleep(2);
+
+                    // установлено
+                    h.sendEmptyMessage(STATUS_CONNECTED);
+
+                    // выполняется какая-то работа
+                    TimeUnit.SECONDS.sleep(3);
+
+                    // разрываем подключение
+                    h.sendEmptyMessage(STATUS_NONE);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start();
     }
 
-    void downloadFile() {
-        // пауза - 1 секунда
-        try {
-            TimeUnit.SECONDS.sleep(1);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 }
