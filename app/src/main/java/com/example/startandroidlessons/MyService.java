614bade1e6e0e96e58ff212767d98208ede79a70
vvvvvvvvvvvvvvvvvvvvvvvvv <31472677+vvvvvvvvvvvvvvvvvvvvvvvvv@users.noreply.github.com>
2021-03-10 18:23:13 +0300

    Pending intent

--- app/src/main/java/com/example/startandroidlessons/MyService.java
+++ app/src/main/java/com/example/startandroidlessons/MyService.java
@@ -1,7 +1,11 @@
 package com.example.startandroidlessons;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+import android.app.PendingIntent;
+import android.app.PendingIntent.CanceledException;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
@@ -10,10 +14,12 @@ import android.util.Log;
 public class MyService extends Service {
 
     final String LOG_TAG = "myLogs";
+    ExecutorService es;
 
     public void onCreate() {
         super.onCreate();
         Log.d(LOG_TAG, "MyService onCreate");
+        es = Executors.newFixedThreadPool(2);
     }
 
     public void onDestroy() {
@@ -23,37 +29,49 @@ public class MyService extends Service {
 
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(LOG_TAG, "MyService onStartCommand");
-        readFlags(flags);
-        MyRun mr = new MyRun(startId);
-        new Thread(mr).start();
-        return START_STICKY;
+
+        int time = intent.getIntExtra(MainActivity.PARAM_TIME, 1);
+        PendingIntent pi = intent.getParcelableExtra(MainActivity.PARAM_PINTENT);
+
+        MyRun mr = new MyRun(time, startId, pi);
+        es.execute(mr);
+
+        return super.onStartCommand(intent, flags, startId);
     }
 
     public IBinder onBind(Intent arg0) {
         return null;
     }
 
-    void readFlags(int flags) {
-        if ((flags&START_FLAG_REDELIVERY) == START_FLAG_REDELIVERY)
-            Log.d(LOG_TAG, "START_FLAG_REDELIVERY");
-        if ((flags&START_FLAG_RETRY) == START_FLAG_RETRY)
-            Log.d(LOG_TAG, "START_FLAG_RETRY");
-    }
-
     class MyRun implements Runnable {
 
+        int time;
         int startId;
+        PendingIntent pi;
 
-        public MyRun(int startId) {
+        public MyRun(int time, int startId, PendingIntent pi) {
+            this.time = time;
             this.startId = startId;
+            this.pi = pi;
             Log.d(LOG_TAG, "MyRun#" + startId + " create");
         }
 
         public void run() {
-            Log.d(LOG_TAG, "MyRun#" + startId + " start");
+            Log.d(LOG_TAG, "MyRun#" + startId + " start, time = " + time);
             try {
-                TimeUnit.SECONDS.sleep(15);
+                // сообщаем об старте задачи
+                pi.send(MainActivity.STATUS_START);
+
+                // начинаем выполнение задачи
+                TimeUnit.SECONDS.sleep(time);
+
+                // сообщаем об окончании задачи
+                Intent intent = new Intent().putExtra(MainActivity.PARAM_RESULT, time * 100);
+                pi.send(MyService.this, MainActivity.STATUS_FINISH, intent);
+
             } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (CanceledException e) {
                 e.printStackTrace();
             }
             stop();
