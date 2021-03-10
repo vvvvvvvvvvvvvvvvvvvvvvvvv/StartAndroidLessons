5adb8c699e8c6c02dc10f534e48afa3247f87900
vvvvvvvvvvvvvvvvvvvvvvvvv <31472677+vvvvvvvvvvvvvvvvvvvvvvvvv@users.noreply.github.com>
2021-03-10 22:30:33 +0300

    Notification

--- app/src/main/java/com/example/startandroidlessons/MainActivity.java
+++ app/src/main/java/com/example/startandroidlessons/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.startandroidlessons;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,97 +10,38 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.google.android.material.button.MaterialButton;
+
 public class MainActivity extends Activity {
-
-    final String LOG_TAG = "myLogs";
-
-    final int TASK1_CODE = 1;
-    final int TASK2_CODE = 2;
-    final int TASK3_CODE = 3;
-
-    public final static int STATUS_START = 100;
-    public final static int STATUS_FINISH = 200;
-
-    public final static String PARAM_TIME = "time";
-    public final static String PARAM_PINTENT = "pendingIntent";
-    public final static String PARAM_RESULT = "result";
-
-    TextView tvTask1;
-    TextView tvTask2;
-    TextView tvTask3;
-
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvTask1 = (TextView) findViewById(R.id.tvTask1);
-        tvTask1.setText("Task1");
-        tvTask2 = (TextView) findViewById(R.id.tvTask2);
-        tvTask2.setText("Task2");
-        tvTask3 = (TextView) findViewById(R.id.tvTask3);
-        tvTask3.setText("Task3");
+
     }
 
     public void onClickStart(View v) {
-        PendingIntent pi;
-        Intent intent;
-        Intent intent1 = new Intent();
-        // Создаем PendingIntent для Task1
-        pi = createPendingResult(TASK1_CODE, intent1, 0);
-        // Создаем Intent для вызова сервиса, кладем туда параметр времени
-        // и созданный PendingIntent
-        intent = new Intent(this, MyService.class).putExtra(PARAM_TIME, 7)
-                .putExtra(PARAM_PINTENT, pi);
-        // стартуем сервис
-        startService(intent);
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
 
-        pi = createPendingResult(TASK2_CODE, intent1, 0);
-        intent = new Intent(this, MyService.class).putExtra(PARAM_TIME, 4)
-                .putExtra(PARAM_PINTENT, pi);
-        startService(intent);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "title")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("My notification")
+                .setContentText("Hello World!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
 
-        pi = createPendingResult(TASK3_CODE, intent1, 0);
-        intent = new Intent(this, MyService.class).putExtra(PARAM_TIME, 6)
-                .putExtra(PARAM_PINTENT, pi);
-        startService(intent);
+// notificationId is a unique int for each notification that you must define
+        notificationManager.notify(1, builder.build());
+
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        Log.d(LOG_TAG, "requestCode = " + requestCode + ", resultCode = "
-                + resultCode);
 
-        // Ловим сообщения о старте задач
-        if (resultCode == STATUS_START) {
-            switch (requestCode) {
-                case TASK1_CODE:
-                    tvTask1.setText("Task1 start");
-                    break;
-                case TASK2_CODE:
-                    tvTask2.setText("Task2 start");
-                    break;
-                case TASK3_CODE:
-                    tvTask3.setText("Task3 start");
-                    break;
-            }
-        }
-
-        // Ловим сообщения об окончании задач
-        if (resultCode == STATUS_FINISH) {
-            int result = data.getIntExtra(PARAM_RESULT, 0);
-            switch (requestCode) {
-                case TASK1_CODE:
-                    tvTask1.setText("Task1 finish, result = " + result);
-                    break;
-                case TASK2_CODE:
-                    tvTask2.setText("Task2 finish, result = " + result);
-                    break;
-                case TASK3_CODE:
-                    tvTask3.setText("Task3 finish, result = " + result);
-                    break;
-            }
-        }
-    }
 }
