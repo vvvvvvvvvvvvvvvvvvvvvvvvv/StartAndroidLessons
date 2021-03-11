28a13ba2e0a585145cdefa40404b29b97ca135db
vvvvvvvvvvvvvvvvvvvvvvvvv <31472677+vvvvvvvvvvvvvvvvvvvvvvvvv@users.noreply.github.com>
2021-03-11 18:10:16 +0300

    Service binding

--- app/src/main/java/com/example/startandroidlessons/MainActivity.java
+++ app/src/main/java/com/example/startandroidlessons/MainActivity.java
@@ -1,123 +1,63 @@
 package com.example.startandroidlessons;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
+import android.content.ComponentName;
 import android.content.Intent;
-import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     final String LOG_TAG = "myLogs";
 
-    final int TASK1_CODE = 1;
-    final int TASK2_CODE = 2;
-    final int TASK3_CODE = 3;
-
-    public final static int STATUS_START = 100;
-    public final static int STATUS_FINISH = 200;
-
-    public final static String PARAM_TIME = "time";
-    public final static String PARAM_TASK = "task";
-    public final static String PARAM_RESULT = "result";
-    public final static String PARAM_STATUS = "status";
-
-    public final static String BROADCAST_ACTION = "ru.startandroid.develop.p0961servicebackbroadcast";
-
-
-    TextView tvTask1;
-    TextView tvTask2;
-    TextView tvTask3;
-    BroadcastReceiver br;
+    boolean bound = false;
+    ServiceConnection sConn;
+    Intent intent;
 
     /** Called when the activity is first created. */
-    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvTask1 = (TextView) findViewById(R.id.tvTask1);
-        tvTask1.setText("Task1");
-        tvTask2 = (TextView) findViewById(R.id.tvTask2);
-        tvTask2.setText("Task2");
-        tvTask3 = (TextView) findViewById(R.id.tvTask3);
-        tvTask3.setText("Task3");
 
-        // создаем BroadcastReceiver
-        br = new BroadcastReceiver() {
-            // действия при получении сообщений
-            public void onReceive(Context context, Intent intent) {
-                int task = intent.getIntExtra(PARAM_TASK, 0);
-                int status = intent.getIntExtra(PARAM_STATUS, 0);
-                Log.d(LOG_TAG, "onReceive: task = " + task + ", status = " + status);
+        intent = new Intent("com.example.servtest.MyService");
+        intent.setPackage("com.example.servtest");
 
-                // Ловим сообщения о старте задач
-                if (status  == STATUS_START) {
-                    switch (task) {
-                        case TASK1_CODE:
-                            tvTask1.setText("Task1 start");
-                            break;
-                        case TASK2_CODE:
-                            tvTask2.setText("Task2 start");
-                            break;
-                        case TASK3_CODE:
-                            tvTask3.setText("Task3 start");
-                            break;
-                    }
-                }
+        sConn = new ServiceConnection() {
+            public void onServiceConnected(ComponentName name, IBinder binder) {
+                Log.d(LOG_TAG, "MainActivity onServiceConnected");
+                bound = true;
+            }
 
-                // Ловим сообщения об окончании задач
-                if (status == STATUS_FINISH) {
-                    int result = intent.getIntExtra(PARAM_RESULT, 0);
-                    switch (task) {
-                        case TASK1_CODE:
-                            tvTask1.setText("Task1 finish, result = " + result);
-                            break;
-                        case TASK2_CODE:
-                            tvTask2.setText("Task2 finish, result = " + result);
-                            break;
-                        case TASK3_CODE:
-                            tvTask3.setText("Task3 finish, result = " + result);
-                            break;
-                    }
-                }
+            public void onServiceDisconnected(ComponentName name) {
+                Log.d(LOG_TAG, "MainActivity onServiceDisconnected");
+                bound = false;
             }
         };
-        // создаем фильтр для BroadcastReceiver
-        IntentFilter intFilt = new IntentFilter(BROADCAST_ACTION);
-        // регистрируем (включаем) BroadcastReceiver
-        registerReceiver(br, intFilt);
     }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        // дерегистрируем (выключаем) BroadcastReceiver
-        unregisterReceiver(br);
-    }
-
-
 
     public void onClickStart(View v) {
-        Intent intent;
-
-        // Создаем Intent для вызова сервиса,
-        // кладем туда параметр времени и код задачи
-        intent = new Intent(this, MyService.class).putExtra(PARAM_TIME, 7)
-                .putExtra(PARAM_TASK, TASK1_CODE);
-        // стартуем сервис
-        startService(intent);
-
-        intent = new Intent(this, MyService.class).putExtra(PARAM_TIME, 4)
-                .putExtra(PARAM_TASK, TASK2_CODE);
-        startService(intent);
-
-        intent = new Intent(this, MyService.class).putExtra(PARAM_TIME, 6)
-                .putExtra(PARAM_TASK, TASK3_CODE);
         startService(intent);
     }
 
+    public void onClickStop(View v) {
+        stopService(intent);
+    }
+
+    public void onClickBind(View v) {
+        bindService(intent, sConn, BIND_AUTO_CREATE);
+    }
+
+    public void onClickUnBind(View v) {
+        if (!bound) return;
+        unbindService(sConn);
+        bound = false;
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        onClickUnBind(null);
+    }
 }
