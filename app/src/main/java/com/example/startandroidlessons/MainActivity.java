933955ac0c5d01798c03c5801b945c50eb39232d
vvvvvvvvvvvvvvvvvvvvvvvvv <31472677+vvvvvvvvvvvvvvvvvvvvvvvvv@users.noreply.github.com>
2021-03-11 11:38:38 +0300

    ServiceWithBroadcast

--- app/src/main/java/com/example/startandroidlessons/MainActivity.java
+++ app/src/main/java/com/example/startandroidlessons/MainActivity.java
@@ -1,47 +1,123 @@
 package com.example.startandroidlessons;
 
 import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-
-import com.google.android.material.button.MaterialButton;
-
 public class MainActivity extends Activity {
+
+    final String LOG_TAG = "myLogs";
+
+    final int TASK1_CODE = 1;
+    final int TASK2_CODE = 2;
+    final int TASK3_CODE = 3;
+
+    public final static int STATUS_START = 100;
+    public final static int STATUS_FINISH = 200;
+
+    public final static String PARAM_TIME = "time";
+    public final static String PARAM_TASK = "task";
+    public final static String PARAM_RESULT = "result";
+    public final static String PARAM_STATUS = "status";
+
+    public final static String BROADCAST_ACTION = "ru.startandroid.develop.p0961servicebackbroadcast";
+
+
+    TextView tvTask1;
+    TextView tvTask2;
+    TextView tvTask3;
+    BroadcastReceiver br;
+
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvTask1 = (TextView) findViewById(R.id.tvTask1);
+        tvTask1.setText("Task1");
+        tvTask2 = (TextView) findViewById(R.id.tvTask2);
+        tvTask2.setText("Task2");
+        tvTask3 = (TextView) findViewById(R.id.tvTask3);
+        tvTask3.setText("Task3");
 
+        // создаем BroadcastReceiver
+        br = new BroadcastReceiver() {
+            // действия при получении сообщений
+            public void onReceive(Context context, Intent intent) {
+                int task = intent.getIntExtra(PARAM_TASK, 0);
+                int status = intent.getIntExtra(PARAM_STATUS, 0);
+                Log.d(LOG_TAG, "onReceive: task = " + task + ", status = " + status);
+
+                // Ловим сообщения о старте задач
+                if (status  == STATUS_START) {
+                    switch (task) {
+                        case TASK1_CODE:
+                            tvTask1.setText("Task1 start");
+                            break;
+                        case TASK2_CODE:
+                            tvTask2.setText("Task2 start");
+                            break;
+                        case TASK3_CODE:
+                            tvTask3.setText("Task3 start");
+                            break;
+                    }
+                }
+
+                // Ловим сообщения об окончании задач
+                if (status == STATUS_FINISH) {
+                    int result = intent.getIntExtra(PARAM_RESULT, 0);
+                    switch (task) {
+                        case TASK1_CODE:
+                            tvTask1.setText("Task1 finish, result = " + result);
+                            break;
+                        case TASK2_CODE:
+                            tvTask2.setText("Task2 finish, result = " + result);
+                            break;
+                        case TASK3_CODE:
+                            tvTask3.setText("Task3 finish, result = " + result);
+                            break;
+                    }
+                }
+            }
+        };
+        // создаем фильтр для BroadcastReceiver
+        IntentFilter intFilt = new IntentFilter(BROADCAST_ACTION);
+        // регистрируем (включаем) BroadcastReceiver
+        registerReceiver(br, intFilt);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // дерегистрируем (выключаем) BroadcastReceiver
+        unregisterReceiver(br);
+    }
+
+
 
     public void onClickStart(View v) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        Intent intent;
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "title")
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle("My notification")
-                .setContentText("Hello World!")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                // Set the intent that will fire when the user taps the notification
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true);
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        // Создаем Intent для вызова сервиса,
+        // кладем туда параметр времени и код задачи
+        intent = new Intent(this, MyService.class).putExtra(PARAM_TIME, 7)
+                .putExtra(PARAM_TASK, TASK1_CODE);
+        // стартуем сервис
+        startService(intent);
 
-// notificationId is a unique int for each notification that you must define
-        notificationManager.notify(1, builder.build());
+        intent = new Intent(this, MyService.class).putExtra(PARAM_TIME, 4)
+                .putExtra(PARAM_TASK, TASK2_CODE);
+        startService(intent);
 
+        intent = new Intent(this, MyService.class).putExtra(PARAM_TIME, 6)
+                .putExtra(PARAM_TASK, TASK3_CODE);
+        startService(intent);
     }
-
 
 }
